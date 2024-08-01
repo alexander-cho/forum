9cc0b71b63e0b9f3ed4489f504e66ef337fe3632
Alex Cho <alex.jhcho@gmail.com>
2024-08-01 16:22:32 -0700

    test: write tests for slugify utility function (articles)

--- articles/models.py
+++ articles/models.py
@@ -22,7 +22,6 @@ class Article(models.Model):
 
 
 def article_pre_save(sender, instance, *args, **kwargs):
-    print('pre_save')
     if instance.slug is None:
         slugify_article_instance_title(instance, save=False)
 
@@ -30,7 +29,6 @@ pre_save.connect(article_pre_save, sender=Article)
 
 
 def article_post_save(sender, instance, created, *args, **kwargs):
-    print('post_save')
     if created:
         slugify_article_instance_title(instance, save=True)
 
--- articles/tests.py
+++ articles/tests.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
-
+from django.utils.text import slugify
 from .models import Article
-from .utils import slugify
+from .utils import slugify_article_instance_title
+
 
 # Create your tests here.
 class TestArticle(TestCase):
@@ -9,19 +10,31 @@ class TestArticle(TestCase):
     Tests for the Article model.
     """
     def setUp(self):
-        self.number_of_articles = 10
+        """
+        set up for each test for consistent state, create Article objects
+        """
+        self.number_of_articles = 100
         for _ in range(0, self.number_of_articles):
             Article.objects.create(title='hello world', content='asdfcsea')
 
     def test_queryset_exists(self):
+        """
+        test that queryset for Article exists; returns something
+        """
         qs = Article.objects.all()
         self.assertTrue(qs.exists())
 
     def test_queryset_count(self):
+        """
+        test that correct number of Article objects has been created
+        """
         qs = Article.objects.all()
         self.assertEqual(qs.count(), self.number_of_articles)
 
     def test_queryset_valid_slug(self):
+        """
+        test slug of Article object matches slugified version of its title
+        """
         obj = Article.objects.all().order_by('id').first()
         title = obj.title
         slug_title = slugify(title)
@@ -30,7 +43,7 @@ class TestArticle(TestCase):
 
     def test_queryset_unique_slug(self):
         """
-        test for unique slugs, which will contain the random number 'tail'
+        test for unique slugs, which will contain the random number ending
         """
         qs = Article.objects.exclude(slug__iexact='hello-world')
         for obj in qs:
@@ -38,3 +51,20 @@ class TestArticle(TestCase):
             slug_title = slugify(title)
             slug = obj.slug
             self.assertNotEqual(slug, slug_title)
+
+    def test_slugify_article_instance_title(self):
+        """
+        test utility function for generating unique slugs
+        """
+        obj = Article.objects.all().last()
+        new_slugs = []
+        for _ in range(0, 25):
+            instance = slugify_article_instance_title(obj, save=False)
+            new_slugs.append(instance.slug)
+        unique_slugs = list(set(new_slugs))
+        self.assertEqual(len(new_slugs), len(unique_slugs))
+
+    def test_slugify_article_instance_title_redux(self):
+        slug_list = Article.objects.all().values_list('slug', flat=True)
+        unique_slug_list = list(set(slug_list))
+        self.assertEqual(len(slug_list), len(unique_slug_list))
