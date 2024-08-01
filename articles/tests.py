70390d7195f5f17dbbe6ce2c87406227da05acc3
Alex Cho <alex.jhcho@gmail.com>
2024-07-31 23:55:23 -0700

    test: Article model

--- articles/tests.py
+++ articles/tests.py
@@ -1,3 +1,40 @@
 from django.test import TestCase
 
+from .models import Article
+from .utils import slugify
+
 # Create your tests here.
+class TestArticle(TestCase):
+    """
+    Tests for the Article model.
+    """
+    def setUp(self):
+        self.number_of_articles = 10
+        for _ in range(0, self.number_of_articles):
+            Article.objects.create(title='hello world', content='asdfcsea')
+
+    def test_queryset_exists(self):
+        qs = Article.objects.all()
+        self.assertTrue(qs.exists())
+
+    def test_queryset_count(self):
+        qs = Article.objects.all()
+        self.assertEqual(qs.count(), self.number_of_articles)
+
+    def test_queryset_valid_slug(self):
+        obj = Article.objects.all().order_by('id').first()
+        title = obj.title
+        slug_title = slugify(title)
+        slug = obj.slug
+        self.assertEqual(slug, slug_title)
+
+    def test_queryset_unique_slug(self):
+        """
+        test for unique slugs, which will contain the random number 'tail'
+        """
+        qs = Article.objects.exclude(slug__iexact='hello-world')
+        for obj in qs:
+            title = obj.title
+            slug_title = slugify(title)
+            slug = obj.slug
+            self.assertNotEqual(slug, slug_title)
--- manager/tests.py
+++ manager/tests.py
@@ -6,7 +6,7 @@ from django.contrib.auth.password_validation import validate_password
 
 class ManagerConfigTest(TestCase):
     def test_secret_key_strength(self):
-        SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY_TEST')
+        SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
         try:
             is_strong = validate_password(SECRET_KEY)
         except Exception as e:
